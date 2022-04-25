d283a20e37e3229e987494b6021ca65334e54a6f
Saif Shahriar <saifshahriar111@gmail.com>
2022-04-25 11:30:44 +0600

    Update truck.php

--- Backend/truck.php
+++ Backend/truck.php
@@ -1,1 +1,270 @@
 
+<html>
+<head>
+<body>
+<style>
+table{
+	border-collapse:collapse;
+	width:100%;
+	color:#588c7e;
+	font-family:monospace;
+	font-size:25px;
+	text-align:center;
+}
+th{
+	background-color:#d96459;
+	color:white;
+}
+tr:nth-child(even){background-color:#f2f2f2;}
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 3px 7px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style>
+	
+<table>
+<tr><th>Number_Plate</th><th>Size</th><th>Price</th><th>Update</th><th>Delete</th></tr>
+<?php
+$hostname='localhost';
+$username='root';
+$password='';
+$dbname='jatra';
+$con=mysqli_connect($hostname,$username,$password,$dbname);
+if($con)
+{
+	echo"conn true";
+}
+else
+{
+	echo"conn false";
+}
+$show="SELECT * FROM truck";
+$q=mysqli_query($con,$show);
+if($q)
+{
+while($data=mysqli_fetch_assoc($q))
+{
+	$Number_Plate=$data['Number_Plate'];
+$Size=$data['Size'];
+$Price=$data['Price'];
+echo"<tr><td>$Number_Plate</td> <td>$Size</td>   <td>$Price</td><td><a href='trucku.php?Number_Plate=$data[Number_Plate]'>edit</td><td><a href='truckd.php?Number_Plate=$data[Number_Plate]'>delete</a></td></tr>";
+}
+}
+?>
+</table>
+</body>
+</head>
+</html>
+<?php
+$hostname='localhost';
+$username='root';
+$password='';
+$dbname='jatra';
+$con=mysqli_connect($hostname,$username,$password,$dbname);
+if($con)
+{
+	echo"conn true";
+}
+else
+{
+	echo"conn false";
+}
+ $Number_Plate= $_GET['Number_Plate'];
+ $sql="SELECT * FROM truck WHERE Number_Plate='$Number_Plate'";
+ $r=mysqli_query($con,$sql);
+ $data=mysqli_fetch_assoc($r);
+ $Number_Plate=$data['Number_Plate'];
+$Size=$data['Size'];
+$Price=$data['Price'];
+ if(isset($_POST['Number_Plate']) && isset($_POST['Size']) && isset($_POST['Price'])){
+    $Number_Plate=$_POST['Number_Plate'];
+    $Size=$_POST['Size'];
+$Price=$_POST['Price'];
+$sql="UPDATE truck SET Number_Plate='$Number_Plate',Size='$Size',Price='$Price' WHERE Number_Plate='$Number_Plate'";
+$r=mysqli_query($con,$sql);
+if($r)
+{
+	echo"update succesfully";
+}
+else
+{
+	echo"update failed";
+}
+
+ }
+?>
+<!DOCTYPE html>
+
+
+<html>
+
+    
+   
+    
+    <style>
+    
+        *{margin: 0; padding: 0;}
+        body{background: #ecf1f4; font-family: sans-serif;}
+        
+        .form-wrap{ width: 320px; background: #3e3d3d; padding: 40px 20px; box-sizing: border-box; position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%);}
+        h1{text-align: center; color: #fff; font-weight: normal; margin-bottom: 20px;}
+        
+        input{width: 100%; background: none; border: 1px solid #fff; border-radius: 3px; padding: 6px 15px; box-sizing: border-box; margin-bottom: 20px; font-size: 16px; color: #fff;}
+        
+        input[type="button"]{ background: #bac675; border: 0; cursor: pointer; color: #3e3d3d;}
+        input[type="button"]:hover{ background: #a4b15c; transition: .6s;}
+        
+        ::placeholder{color: #fff;}
+    
+    </style>
+
+    <body>
+    
+        <div class="form-wrap">
+        
+            <form action="" method="POST">
+            
+                <h1>Truck</h1>
+
+                <input type="text" placeholder=Number_Plate value="<?php echo"$Number_Plate"?>" name="Number_Plate">
+
+               <input type="text" placeholder=Size value="<?php echo"$Size"?>" name="Size">
+
+<input type="number_format" placeholder=Price value="<?php echo"$Price"?>" name="Price">
+
+<input type="submit" placeholder=submit value="update">
+
+            
+            </form>
+        
+        </div>
+    
+    
+    
+    </body>
+
+
+
+</html>
+
+
+<?php
+$hostname='localhost';
+$username='root';
+$password='';
+$dbname='jatra';
+$con=mysqli_connect($hostname,$username,$password,$dbname);
+if($con)
+{
+	echo"conn true";
+}
+else
+{
+	echo"conn false";
+}
+if(isset($_POST['Number_Plate']) && isset($_POST['Size']) && isset($_POST['Price'])){
+    $Number_Plate=$_POST['Number_Plate'];
+    $Size=$_POST['Size'];
+$Price=$_POST['Price'];
+$insert="INSERT INTO truck(Number_Plate,Size,Price)VALUES('$Number_Plate','$Size','$Price')";
+
+if(mysqli_query($con,$insert))
+{
+echo"true";
+}
+
+	else
+	{
+		echo"false";
+	}
+}
+?>
+<!DOCTYPE html>
+
+
+<html>
+
+    
+   
+    
+    <style>
+    
+        *{margin: 0; padding: 0;}
+        body{background: #ecf1f4; font-family: sans-serif;}
+        
+        .form-wrap{ width: 320px; background: #3e3d3d; padding: 40px 20px; box-sizing: border-box; position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%);}
+        h1{text-align: center; color: #fff; font-weight: normal; margin-bottom: 20px;}
+        
+        input{width: 100%; background: none; border: 1px solid #fff; border-radius: 3px; padding: 6px 15px; box-sizing: border-box; margin-bottom: 20px; font-size: 16px; color: #fff;}
+        
+        input[type="button"]{ background: #bac675; border: 0; cursor: pointer; color: #3e3d3d;}
+        input[type="button"]:hover{ background: #a4b15c; transition: .6s;}
+        
+        ::placeholder{color: #fff;}
+    
+    </style>
+
+    <body>
+    
+        <div class="form-wrap">
+        
+            <form action="" method="POST">
+            
+                <h1>TRUCK</h1>
+                <input type="text" placeholder=Number_Plate name="Number_Plate">
+
+               <input type="text" placeholder=Size name="Size">
+
+<input type="number_format" placeholder=price name="Price">
+
+<input type="submit" placeholder=submit value="submit">
+
+            
+            </form>
+        
+        </div>
+    
+    
+    
+    </body>
+
+
+
+</html>
+
+<?php
+$hostname='localhost';
+$username='root';
+$password='';
+$dbname='jatra';
+$con=mysqli_connect($hostname,$username,$password,$dbname);
+if($con)
+{
+	echo"conn true";
+}
+else
+{
+	echo"conn false";
+}
+$Number_Plate=$_GET['Number_Plate'];
+$d="DELETE FROM truck WHERE Number_Plate='$Number_Plate'";
+$q=mysqli_query($con,$d);
+if($q)
+{
+	echo"<font color='green'>Deleted from database";
+}
+else
+{
+	echo"<font color='red'>Failed to delete";
+}
+?>
+
+	
